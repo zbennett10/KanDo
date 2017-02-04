65b9610f5f7952a52e3ba57284e7828fc2fcf734
Zachary Bennett <zbennett10@hotmail.com>
2017-02-03 19:51:01 -0500

    commenting

--- src/reducers/todo_reducer.js
+++ src/reducers/todo_reducer.js
@@ -23,10 +23,10 @@ export default function(state = initialTodo, action) {
         case 'CLEAR_TODO':
             return action.payload;
         case 'UPDATE_TODO':
-            let updatedIndex = state.indexOf(state.find(project=>project.id===action.payload.id));
-            let newState = state.slice();
-            newState.splice(updatedIndex, 1, action.payload);
-            return newState;//replace old project with new project here;
+            let updatedIndex = state.indexOf(state.find(project=>project.id===action.payload.id)); //find index of project in state array
+            let newState = state.slice(); //create copy of current state
+            newState.splice(updatedIndex, 1, action.payload); //replace old project with new project
+            return newState;
         default:
             return state;
     }
